5efa47aecaede5980e34e20fe3ad76fc5251b7da
Mark Kirby <markkirby1997@gmail.com>
2026-03-13 11:37:47 -0400

    Modified the navigation bar to adhere to the structure in the example referenced.

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -4,7 +4,7 @@ import data from "../../app/data/navigation.json"
 export default function NavBar(){
 
     return(
-        <div className="w-full flex flex-row justify-center gap-5">
+        <div className="w-full flex flex-row justify-center gap-12">
             {data.navigation.map((item,idx) =>{
                 return <NavItem key={`${item.label}-${idx}`} idx={idx} label={item.label} url={item.url}/>
             })}
