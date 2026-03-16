0b96261b52899f1e18d66b2c678a7f4e4f1da738
Mark Kirby <markkirby1997@gmail.com>
2026-03-15 22:16:06 -0400

    Changed the website basic structure - removed unneccessary classes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,15 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-mist-950 lg:p-8 md:p-6 p-4`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-mist-950 lg:p-8 md:p-6 p-4 w-full`}>
         <Header id="social-media-n-mobile-header" />
-        <div className="font-sans flex flex-col justify-start items-center min-h-screen">
-          {/** Navigation */}
-          {/**<Header/>**/}
-          {/** Main withg children pages */}
+        <div className="font-sans flex flex-col justify-start items-center min-h-screen w-full">
           {children}
         </div>
-        {/** Footer */}
         <Footer />
         <ParticlesBackground />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import Work from "@/components/main/works/work"
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-center">
+    <>
         <Hero id="heroSection" />
         <Skills id="mySkills" />
         <Work id="work" />
         <Experience id="experience" />
-    </div>
+    </>
   );
 }
