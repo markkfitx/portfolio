797ae4792f984e78ef88fd97a6e4dc86b15e3f92
Mark Kirby <markkirby1997@gmail.com>
2026-03-15 20:19:16 -0400

    Added new header section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../app/css/globals.css";
 import "../app/css/layout.css";
-import Header from "@/components/header"
+import Header from "@/components/header/header"
 import Footer from "@/components/footer/footer"
 import ParticlesBackground from "@/components/particle-background";
 
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-mist-950`}>
+        <Header id="social-media-n-mobile-header" />
         <div className="font-sans flex flex-col justify-start items-center min-h-screen">
           {/** Navigation */}
           {/**<Header/>**/}
