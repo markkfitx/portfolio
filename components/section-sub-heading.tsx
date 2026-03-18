b139ee71d8194ee57b56314ae71a746a36bdfd25
Mark Kirby <markkirby1997@gmail.com>
2026-03-17 22:03:07 -0400

    Added a new Subheading component to replace the main hero-heading component.

---
+++ components/section-sub-heading.tsx
@@ -0,0 +1,39 @@
+import {Badge} from "./ui/badge"
+import clsx from "clsx"
+interface contentProps{
+    id:string,
+    alignment:"start" | "center" | "end",
+    className?:string,
+    children?: React.ReactNode
+}
+export default function SectionHero({id,alignment,className, children} : contentProps){
+    const alignmentStyles = {
+        start: {
+            container: "items-start",
+            text: "text-left",
+        },
+        center: {
+            container: "items-center",
+            text: "text-center",
+        },
+        end: {
+            container: "items-end",
+            text: "text-right",
+        },
+    } as const
+
+    return (
+        <div
+          id={id}
+          className={clsx(
+            "w-full mb-8 z-10 ",
+            alignmentStyles[alignment].container,
+            className
+            )}
+          >
+            <h3 className={clsx("text-white z-10 w-full md:max-w-[500px]",alignmentStyles[alignment].text)}>
+              {children}
+            </h3>
+        </div>
+    )
+}
